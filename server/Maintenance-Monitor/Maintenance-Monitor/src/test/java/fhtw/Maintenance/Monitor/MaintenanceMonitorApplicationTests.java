55e73d631b47693f7ccc3f071a72c781da834081
Florian <florian-parzer@gmx.at>
2022-10-25 18:58:51 +0200

    Spring Boot Projekt erstellt

---
+++ server/Maintenance-Monitor/Maintenance-Monitor/src/test/java/fhtw/Maintenance/Monitor/MaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.Maintenance.Monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
